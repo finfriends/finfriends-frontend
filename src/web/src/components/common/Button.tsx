1c63de9269bf3f5a61d89ced94cdd51ef2e33347
Carrole <eundori.dev@gmail.com>
2024-09-15 16:10:37 +0900

    feat: button component 구현

---
+++ src/web/src/components/common/Button.tsx
@@ -0,0 +1,76 @@
+import React, { ReactNode } from 'react';
+import styled from '@emotion/styled';
+import { ButtonTheme } from '@/constants/button';
+import { Color } from '@/styles/color';
+import { Typography } from '@/styles/fonts';
+
+const Button = ({
+  theme,
+  label,
+  isDisabled,
+  onClick,
+  icon,
+}: {
+  theme: ButtonTheme;
+  label: string;
+  isDisabled?: boolean;
+  icon?: ReactNode;
+  onClick?: () => void;
+}) => {
+  return (
+    <S.Button theme={theme} disabled={isDisabled} onClick={onClick}>
+      {icon}
+      {label}
+    </S.Button>
+  );
+};
+
+export default Button;
+
+namespace S {
+  export const Button = styled.button`
+    width: 100%;
+    padding: 12px 24px;
+    border-radius: 8px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    gap: 8px;
+    ${Typography.B1Medium};
+    &:disabled {
+      background: ${Color.ButtonSecond};
+      color: ${Color.Disabled};
+      border: 0;
+    }
+    ${(props) => {
+      switch (props.theme) {
+        case ButtonTheme.Primary:
+          return `
+            background: ${Color.MainBlue3};
+            color: ${Color.Text000};
+            border: 0;
+            `;
+        case ButtonTheme.Secondary:
+          return `
+          background: ${Color.ButtonSecond};
+          color: ${Color.Text800};
+          border: 0;
+          `;
+        case ButtonTheme.Outline:
+          return `
+            background: ${Color.White};
+            border: 1.5px solid ${Color.LineBasic};
+            color: ${Color.Text600};
+            `;
+        case ButtonTheme.Ghost:
+          return `
+            background: ${Color.White};
+            color: ${Color.Text700};
+            border: 0;
+            `;
+        default:
+          return ``;
+      }
+    }}
+  `;
+}
