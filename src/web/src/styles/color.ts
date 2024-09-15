87c52e9d80b23cd786a8e7b81b1372ff22ac87ae
Carrole <eundori.dev@gmail.com>
2024-09-15 16:11:28 +0900

    feat: color 객체를 enum에서 오브젝트로 변경함

--- src/web/src/styles/color.ts
+++ src/web/src/styles/color.ts
@@ -1,29 +1,37 @@
-export enum Color {
-  Text900 = '#1F222D',
-  Text800 = '#2A303C',
-  Text700 = '#4A515F',
-  Text600 = '#5C6272',
-  Text500 = '#798493',
-  Disabled = '#BBC0C9',
-  Text000 = '#FFFFFF',
+export const Color = {
+  White: '#FFF',
 
-  LineBasic = '#EAECF0',
-  LineSecond = '#DEE0E4',
+  Text900: '#1F222D',
+  Text800: '#2A303C',
+  Text700: '#4A515F',
+  Text600: '#5C6272',
+  Text500: '#798493',
+  Text000: '#FFFFFF',
 
-  BackgroundBasic = '#F6F7F8',
+  LineBasic: '#EAECF0',
+  LineSecond: '#DEE0E4',
+  ButtonSecond: '#EFF0F1',
 
-  MainBlue3 = '#52B6EC',
-  MainBlue3_32 = '#52B6EC52',
-  MainBlue3_16 = '#52B6EC29',
-  MainBlue2 = '#5DCBEE',
-  MainBlue1 = '#6AE5ED',
+  IconAbled: '#4A515F',
+  IconSecond: '#5C6272',
+  IconThird: '#798493',
+  Disabled: '#BBC0C9',
 
-  Red = '#F76A68',
-  Red12 = '#F76A681F',
-  Yellow = '#FFC759',
-  Yellow32 = '#FFC75952',
-  Yellow24 = '#FFC7593D',
-  Yellow8 = '#FFC75914',
+  BackgroundBasic: '#F6F7F8',
+  BackgroundDarkBox: '#154D6CB8',
 
-  PendingSecond = '#D1D4DA',
-}
+  MainBlue3: '#52B6EC',
+  MainBlue3_32: '#52B6EC52',
+  MainBlue3_16: '#52B6EC29',
+  MainBlue2: '#5DCBEE',
+  MainBlue1: '#6AE5ED',
+
+  Red: '#F76A68',
+  Red12: '#F76A681F',
+  Yellow: '#FFC759',
+  Yellow32: '#FFC75952',
+  Yellow24: '#FFC7593D',
+  Yellow8: '#FFC75914',
+
+  PendingSecond: '#D1D4DA',
+};
