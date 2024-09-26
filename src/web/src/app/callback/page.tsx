06961251dfe18392db5d38345c8dfe5d6a781efc
Carrole <eundori.dev@gmail.com>
2024-09-26 18:56:57 +0900

    fix: suspense 사용을 통해 빌드 오류 해결

--- src/web/src/app/callback/page.tsx
+++ src/web/src/app/callback/page.tsx
@@ -1,18 +1,28 @@
 'use client';
 
+import { Suspense, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useCreateUserMutation } from '@/queries/useAuthQueries';
-import { useEffect } from 'react';
 import { AuthProvider } from '@/constants/auth';
 
-export default function Home() {
+function CallbackComponent() {
   const params = useSearchParams();
   const code = params.get('code');
-  const { mutate } = useCreateUserMutation({});
+  const { mutate } = useCreateUserMutation();
 
   useEffect(() => {
     if (code) {
       mutate({ code: String(code), authProvider: AuthProvider.KAKAO });
     }
   }, [code, mutate]);
+
+  return null;
+}
+
+export default function Home() {
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <CallbackComponent />
+    </Suspense>
+  );
 }
