85d6e6476e4d45d09cb4e03041cf17a351e5e609
Carrole <eundori.dev@gmail.com>
2024-10-07 09:10:57 +0900

    feat: 기록 테이블

--- src/web/src/app/training/table/page.tsx
+++ src/web/src/app/training/table/page.tsx
@@ -1,5 +1,54 @@
 'use client';
 
+import styled from '@emotion/styled';
+import { Color } from '@/styles/color';
+import { Typography } from '@/styles/fonts';
+import { LeftArrowIcon } from '@/icon/LeftArrowIcon';
+import { RecordTable } from '@/components/training/table/RecordTable';
+import { useRouter } from 'next/navigation';
+
 export default function TablePage() {
-  return <div>유저 트레이닝 기록 테이블</div>;
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back();
+  };
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <button type="button" onClick={handleBackClick}>
+          <LeftArrowIcon />
+        </button>
+        <S.Title>테이블 기록</S.Title>
+      </S.Header>
+      <RecordTable />
+    </S.Wrapper>
+  );
+}
+
+namespace S {
+  export const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    padding-bottom: 34px;
+    box-sizing: border-box;
+    flex-direction: column;
+  `;
+
+  export const Header = styled.div`
+    padding: 12px 20px 8px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    position: relative;
+  `;
+
+  export const Title = styled.div`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    ${Typography.H4Medium};
+    color: ${Color.White};
+  `;
 }
---
+++ src/web/src/utils/dateFormatter.ts
@@ -0,0 +1,8 @@
+export const formatDateToYYMMDD = (timeStamp: string): string => {
+  const date = new Date(timeStamp);
+  const year = String(date.getFullYear()).slice(-2);
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
+};
