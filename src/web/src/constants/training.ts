2da01fe4232cedb8c7d4ef3d6d46af89a1214e2c
Carrole <dsmthf200099@gmail.com>
2024-12-10 21:13:49 +0900

    chore: 유저 트레이닝 설정에서 사용하는 option constant화

--- src/web/src/constants/training.ts
+++ src/web/src/constants/training.ts
@@ -3,3 +3,18 @@ export const enum TrainingType {
   BreathBased = 'BreathBased',
   Static = 'Static',
 }
+
+export const TotalRoundOption = [
+  { label: '3 라운드', value: 3 },
+  { label: '4 라운드', value: 4 },
+  { label: '6 라운드', value: 6 },
+  { label: '8 라운드 (기본값)', value: 8 },
+];
+
+export const PreparatoryDurationOption = [
+  { label: '3 (들숨 1초+ 날숨 2초)', value: 3 },
+  { label: '6 (들숨 2초+ 날숨 4초)', value: 6 },
+  { label: '9 (들숨 3초+ 날숨 6초 - 기본 값)', value: 9 },
+  { label: '12 (들숨 4초+ 날숨 8초)', value: 12 },
+  { label: '15 (들숨 5초+ 날숨 10초)', value: 15 },
+];
