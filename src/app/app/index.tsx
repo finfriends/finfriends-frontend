8b07a3e238984ceb12a88e2863938812ac06e17b
Carrole <eundori.dev@gmail.com>
2025-03-04 20:50:41 +0900

    feat: rn prebuild

--- src/app/app/index.tsx
+++ src/app/app/index.tsx
@@ -1,7 +1,29 @@
-import WebViewCont from 'app/components/WebViewCont';
+import { Button, View, Alert } from 'react-native';
+
 
 export default function Index() {
+  const showAlert = () =>
+    Alert.alert(
+      '알림 제목',
+      '안녕하세용가리',
+      [
+        {
+          text: '확인',
+          onPress: () => Alert.alert('확인하셨나용?'),
+          style: 'default',
+        },
+      ],
+    );
+
   return (
-    <WebViewCont />
+    <View>
+      <Button
+        onPress={showAlert}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+    // <WebViewCont />
   );
 }
